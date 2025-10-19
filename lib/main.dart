06e10798065e4ed04279bd7f4680c72bcd944481
sayanlp99 <sayan.chakraborty1999@gmail.com>
2025-10-19 23:18:58 +0530

    Add Firebase integration with configuration and dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:envqmon/app.dart';
+import 'package:envqmon/firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  
+  // Initialize Firebase
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  
   runApp(const EnvQMonApp());
 }
